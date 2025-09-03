3cb20f0aab6f4fdc6e7e596f0a031d5a2fe8c6b8
aldilukmn <aldilukman99@gmail.com>
2025-09-03 21:42:00 +0700

    fix: responsively to the main menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import TextType from './reactbits/Text-Type/TextType';
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center gap-10 h-[100vh]'>
-      <img src="https://res.cloudinary.com/dhtfq9yw8/image/upload/v1717920310/uptd%20sdn%202%20kalimati/svg/vapqm0latukpxjjawzfu.svg" alt="logo-sekolah" className='logo' />
-      <h1 className='text-7xl font-bold tracking-widest'>
+      <img src="https://res.cloudinary.com/dhtfq9yw8/image/upload/v1717920310/uptd%20sdn%202%20kalimati/svg/vapqm0latukpxjjawzfu.svg" alt="logo-sekolah" className='max-w-[35%] h-auto' />
+      <h1 className='xl:text-7xl lg:text-5xl md:text-3xl sm:text-xl text-base font-bold tracking-widest'>
         <TextType 
           text={["Selamat Datang di", "UPTD SD Negeri 2 Kalimati", "Kecamatan Jatibarang"]}
           typingSpeed={75}
