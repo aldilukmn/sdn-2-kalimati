c20cc941bd78026cacc745e0c6bcc1a29e5e2954
unknown <aldilukman99@gmai>
2025-09-04 14:01:52 +0700

    update: change title name and add class menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import TextType from './reactbits/Text-Type/TextType';
 
 export default function Home() {
@@ -13,6 +14,14 @@ export default function Home() {
           cursorCharacter="|"
         />
       </h1>
+      <div className='bg-white flex px-5 py-4 gap-5 rounded-lg shadow-lg items-center'>
+        <Link href={'kelas-1'} className='bg-blue-300 p-2 rounded-lg transition-all duration-200 ease-in-out hover:-translate-y-1 hover:scale-110'>Kelas 1</Link>
+        <Link href={'kelas-2'}>Kelas 2</Link>
+        <Link href={'kelas-3'}>Kelas 3</Link>
+        <Link href={'kelas-4'}>Kelas 4</Link>
+        <Link href={'kelas-5'}>Kelas 5</Link>
+        <Link href={'kelas-6'}>Kelas 6</Link>
+      </div>
     </div>
   );
 }
