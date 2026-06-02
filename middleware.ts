76f367ce0e22550d4a53adf4768bd5aac1de1042
aldilukmn <aldilukman99@gmail.com>
2026-06-02 09:54:41 +0700

    update graduation time

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // Tanggal dan jam kelulusan: 02 Juni 2026 jam 9 pagi
-const GRADUATION_ANNOUNCEMENT_DATE = new Date(2026, 5, 2, 9, 0, 0); // Juni adalah bulan ke-5 (0-indexed)
+const GRADUATION_ANNOUNCEMENT_DATE = new Date(2026, 5, 2, 11, 0, 0); // Juni adalah bulan ke-5 (0-indexed)
 
 export function middleware(request: NextRequest) {
   const now = new Date();
